2dc8ac549554dc7384c504d36646f9d3e1e08b8e
Fejyte <44175797+Fejyte@users.noreply.github.com>
2018-10-28 00:50:14 +0100

    Update q4.py

--- q4.py
+++ q4.py
@@ -3,8 +3,29 @@
 import numpy as np
 
 #compute all combinations for two portfolios
-def question04(rows, numberMachines):
-  # modify and then return the variable below
-  answer = -1
-  return answer
-
+def question04(tin,num):
+    summ = 1000000
+    for x in tin:
+        en = len(x)
+        if en < num or en = num and "X" in x  :
+            continue
+        for i in x:
+            i = 0
+            count = 0
+            summm = 0
+            while  count < num :
+                if i == en:
+                    summm = summ
+                    break
+                if x[i] == "X":
+                    count = 0
+                    summm = 0
+                else:
+                    count += 1
+                    summm += x[i]
+                i += 1
+            if summm < summ:
+                summ = summm
+    if summ == 1000000:
+        return 0
+    return summ
